00e790116a797df61d569364834685fdf478607f
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-11-01 15:03:51 +0300

    Lesson 1 Introduction

---
+++ index.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+console.log("Operating System:",os.platform())
+console.log("Total Memory:", os.totalmem());
+console.log("Free Memory:", os.freemem());
+
+console.log("Node.js is running successfully!");
