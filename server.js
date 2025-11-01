2f8c6f26932b84ec52f98ae37de7e2ccb44d9843
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-11-01 15:14:41 +0300

    route to the about page

--- server.js
+++ server.js
@@ -11,3 +11,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get('/about', (req,res) => {
+  res.send('This is the About page.')
+})
