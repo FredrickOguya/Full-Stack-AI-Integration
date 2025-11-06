4ef83d59dab1bfb398ff790bfc2d2d7713d953e1
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-11-06 14:25:51 +0300

    first route and starting the server

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const os = require('os');
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-console.log("Operating System:",os.platform())
-console.log("Total Memory:", os.totalmem());
-console.log("Free Memory:", os.freemem());
+//Middleware to allow json data
+app.use(express.json());
 
-console.log("Node.js is running successfully!");
+// GET route -for reading data
+app.get('/hello', (req,res) => {
+  const name = req.query.name;// read from URL
+  res.send(`Hello, ${name}!`);
+});
+
+//POST route - for sending data
+app.post('/user', (req,res) => {
+  const {name, age} = req.body; // read JSON data
+  res.json({message: `User ${name}, age ${age}, added successfully`})
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
