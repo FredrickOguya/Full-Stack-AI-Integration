2f116c6bf289daae352b9ee5dc5c8e43f6280737
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-11-06 14:49:22 +0300

    trying to handle server.js

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const app = express();
-
-//Define a simple route
-app.get('/', (req,res) => {
-  res.send('Hello from Express.js!');
-});
-
-// Start the server
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+//Middleware to parse
+app.use(express.json());
+
+//GET route with query params
+app.get('/user', (req,res) => {
+  const name = req.query.name;
+  res.send(`Hello, ${name}!`);
 });
 
-app.get('/about', (req,res) => {
-  res.send('This is the About page.')
+//POST route
+app.post('/user', (req,res) => {
+  const {name, age} = req.body;
+  res.json({message: `User ${name}, age ${age}, added successfully.`});
 });
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-})
+app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
