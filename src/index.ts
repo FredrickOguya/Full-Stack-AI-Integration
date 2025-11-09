50b6e5fb325f30e4c89d21219b41e3c2a4d6d7bb
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-11-09 13:39:05 +0300

    routers in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,14 @@
-import express ,{ type Request,type Response,type NextFunction } from "express"
+import express from "express";
+import { configDotenv } from "dotenv";
+import UserRoutes from "./routes/users.js";
+
+configDotenv();
 
 const app = express();
-
 app.use(express.json());
 
-//custom middleware
-app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log(`${req.method} request to ${req.url}`);
-  next();
-})
+// Mount the users router
+app.use("/users", UserRoutes);
 
-// Route with potential error
-app.get("/divide", (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const a = Number(req.query.a);
-    const b = Number(req.query.b);
-
-    if (isNaN(a) || isNaN(b)) throw new Error("Invalid numbers");
-    if (b === 0) throw new Error("Cannot divide by zero");
-
-    res.json({ result: a / b });
-  } catch (err) {
-    next(err); // pass error to global handler
-  }
-});
-
-// Global error handler
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error("Error:", err.message);
-  res.status(400).json({
-    success: false,
-    message: err.message || "Something went wrong",
-  });
-});
-
-
-app.listen(3000, ()=> {
-  console.log(`Server running on port ${3000}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
