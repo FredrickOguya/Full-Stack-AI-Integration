83923569170fd612b13eb174c44d8928140e383d
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-11-09 12:53:26 +0300

    Middleware && error handling

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
-import express, { type Request,type Response } from 'express';
-import { configDotenv } from 'dotenv';
-
-configDotenv();
+import express ,{ type Request,type Response,type NextFunction } from "express"
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-//middleware to parse json
 app.use(express.json());
 
-//GET route
-app.get("/",(req: Request,res:Response) => {
-  const name = req.query.name as string || "Guest";
-  res.send(`Hello, ${name}!`);
+//custom middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`${req.method} request to ${req.url}`);
+  next();
+})
+
+// Route with potential error
+app.get("/divide", (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+
+    if (isNaN(a) || isNaN(b)) throw new Error("Invalid numbers");
+    if (b === 0) throw new Error("Cannot divide by zero");
+
+    res.json({ result: a / b });
+  } catch (err) {
+    next(err); // pass error to global handler
+  }
 });
 
-app.post("/user",(req: Request,res: Response) => {
-  const {name, age } = req.body as { name: string; age: number};
-  res.json({message: `User ${name}, age ${age}, added successfully.`});
+// Global error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("Error:", err.message);
+  res.status(400).json({
+    success: false,
+    message: err.message || "Something went wrong",
+  });
 });
 
-app.listen(PORT, ()=> {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(3000, ()=> {
+  console.log(`Server running on port ${3000}`)
 })
