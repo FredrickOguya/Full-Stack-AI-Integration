462fff68140d963220881566db16c7b192d58623
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-11-09 11:54:49 +0300

    first succesfull run

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, {Request,Response} from "express";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Express with TypeScript!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
